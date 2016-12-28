6d108993aaf83ea916836ef353d132a0f5eb9253
lokeshreddy <lokeshreddy@lokeshreddys-Mac.local>
2016-12-28 18:51:02 -0500

    first commit

--- Scrollview/ViewController.swift
+++ Scrollview/ViewController.swift
@@ -10,14 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth : CGFloat = 0.0
+        
+        var scrollwidth = scrollView.frame.width
+        for x in 0...2 {
+            
+            
+            
+            
+            let image = UIImage(named: "icon\(x).png")
+            //to give image reference
+            var imagereference = UIImageView(image: image)
+            images.append(imagereference)
+            
+            
+            var newX : CGFloat = 0.0
+            
+            newX = scrollwidth/2 + scrollwidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            print(contentWidth)
+            scrollView.addSubview(imagereference)
+            
+            imagereference.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2)-75, width: 150, height: 150)
+            
+        }
+        scrollView.contentSize = CGSize(width: contentWidth, height: scrollView.frame.size.height)
+        scrollView.clipsToBounds = false
+        print("count is\(images.count)")
     }
 
 
